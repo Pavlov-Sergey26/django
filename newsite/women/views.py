2ad3821542ffc9ac46f02d8726ac991b91a2cd03
Sergei <pavlovergey579@gmail.com>
2023-02-24 22:19:33 +0300

    model links

--- newsite/women/views.py
+++ newsite/women/views.py
@@ -11,9 +11,13 @@ menu = [{'title':'About Site', 'url_name':'about'},
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {'posts':posts, 
                 'menu':menu, 
-                'title':'Main page'}
+                'cats':cats,
+                'title':'Main page',
+                'cat_selected':0
+                }
     return render(request, 'women/index.html', context=context)
 
 def about(request):
@@ -31,6 +35,20 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображения статьи с id={post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+        
+    context = {'posts':posts, 
+                'menu':menu, 
+                'cats':cats,
+                'title':'Display category',
+                'cat_selected':cat_id
+                }
+    return render(request, 'women/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound(f'<h1>Cтраница не найдена</h1>')
